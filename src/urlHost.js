57e27c33ee9c9bf39d2f00667262f8e9d02b69bf
Charley <charley1135@gmail.com>
2023-02-23 10:19:28 -0800

    fix: Added url for production.

---
+++ src/urlHost.js
@@ -0,0 +1,4 @@
+export const URL_HOST =
+  process.env.NODE_ENV === "production"
+    ? "https://cors-everywhere-me.herokuapp.com/http://finfitnessbackend-env.eba-55w3f9b3.us-west-1.elasticbeanstalk.com"
+    : "http://127.0.0.1:8000";
